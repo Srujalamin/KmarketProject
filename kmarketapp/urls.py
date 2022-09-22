9f0145d68130c3ab3b6e562e4962baca3d7cf24a
srujalamin <srujalamin176@gmail.com>
2022-09-22 11:34:38 +0530

    updet

--- kmarketapp/urls.py
+++ kmarketapp/urls.py
@@ -6,6 +6,11 @@ urlpatterns = [
     path('login_page/', login_page, name='login_page'),
     path('forget_password_page/', forget_password_page, name='forget_password_page'),
     path('profile_page/', profile_page, name='profile_page'),
-] 
+    path('signup/', signup, name='signup'),
+    path('login/', login, name='login'),
+    path('profile_update/', profile_update, name='profile_update'),
+    path('logout/', logout, name='logout'),
+]
+    
 
 
--- kmarketapp/views.py
+++ kmarketapp/views.py
@@ -9,12 +9,18 @@ def signup_page(request):
     return render(request, "signup_page.html", default_data)
 
 def login_page(request):
+    if 'email' in request.session:
+        return redirect(profile_page)
+        
     return render(request, "login_page.html", default_data)
 
 def forget_password_page(request):
     return render(request, "forget_password_page.html", default_data)
 
 def profile_page(request):
+    if 'email' not in request.session:
+        return redirect(login_page)
+    profile_data(request) # call profile data
     return render(request, "profile_page.html", default_data)
 
 # get data from signup page
@@ -37,8 +43,52 @@ def signup(request):
 
     return redirect(signup_page)
 
+# profile data
+def profile_data(request):
+    master = Master.objects.get(Email = request.session['email'])
+    profile = UserProfile.objects.get(Master = master)
+
+    default_data['profile_data'] = profile
+
+# profile update functionality
+def profile_update(request):
+    master = Master.objects.get(Email = request.session['email'])
+    profile = UserProfile.objects.get(Master = master)
+
+    profile.FullName = request.POST['full_name']
+    profile.Gender = request.POST['gender']
+    profile.Mobile = request.POST['mobile']
+    profile.BirthDate = request.POST['birth_date']
+    profile.Country = request.POST['country']
+    profile.State = request.POST['state']
+    profile.City = request.POST['city']
+    profile.Address = request.POST['address']
+
+    profile.save()
+
+    print('profile updated successfully.')
+
+    return redirect(profile_page)
+
 # login functionality
 def login(request):
-    master = Master.objects.get(Email = request.POST['email'])
-    default_data['master']=master
-    return redirect(profile_page)
+    try:
+        master = Master.objects.get(Email = request.POST['email'])
+        if master.Password == request.POST['password']:
+            request.session['email'] = master.Email
+            
+            return redirect(profile_page)
+        else:
+            print('incorrect password')
+
+    except Master.DoesNotExist as err:
+        print('something went wrong', err)
+    
+    return redirect(login_page)
+
+# logout 
+def logout(request):
+    if 'email' in request.session:
+        del request.session['email']
+    
+    return redirect(login_page)
